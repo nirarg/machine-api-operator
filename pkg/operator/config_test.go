22069b5d4e3c6dccd19e15844d637a4060c00fa7
Nir Argaman <nargaman@redhat.com>
2020-10-01 13:18:43 +0300

    temporary images with nargaman tag

--- pkg/operator/config_test.go
+++ pkg/operator/config_test.go
@@ -24,7 +24,7 @@ var (
 	expectedIronicStaticIpManager     = "quay.io/openshift/origin-ironic-static-ip-manager:v4.2.0"
 	expectedOvirtImage                = "quay.io/openshift/origin-ovirt-machine-controllers"
 	expectedVSphereImage              = "docker.io/openshift/origin-machine-api-operator:v4.0.0"
-	expectedKubevirtImage             = "quay.io/openshift/origin-kubevirt-machine-controllers"
+	expectedKubevirtImage             = "docker.io/kubevirtplatformipi/cluster-api-provider-kubevirt:nargaman"
 )
 
 func TestGetProviderFromInfrastructure(t *testing.T) {
